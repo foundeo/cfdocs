62c6303dc5414bb627dac1defc8f45a4eb19f6d8
Sebastian Hädrich <shaedrich@users.noreply.github.com>
2017-10-24 16:52:10 +0200

    Delete github.js

--- assets/github.js
+++
@@ -1,6 +0,0 @@
-if(!!$('.issuecount').length) {
-	docname = $('ol.container').data('doc');
-	$.getJSON('https://api.github.com/search/issues?q=is:open+repo:foundeo/cfdocs+' + docname,function(res) {
-		$('.issuecount').html(res.total_count);
-	});
-}
